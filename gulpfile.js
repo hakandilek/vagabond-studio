93852918c712409356667df0199f7f651a1ecdb2
Hakan Dilek <hakandilek@gmail.com>
2016-04-10 00:05:38 +0200

    nomnoml gulp compilation

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ gulp.task("install", function() {
     .pipe(install());
 });
 
-gulp.task('serve', ['webpack'], function() {
+gulp.task('serve', ['nomnoml', 'webpack'], function() {
 
   var browserSync = require('browser-sync').create(),
     reload = browserSync.reload;
@@ -34,5 +34,37 @@ gulp.task("webpack", function() {
   return gulp.src('src/main.js')
     .pipe(webpack(require('./webpack.config.js')))
     .pipe(gulp.dest('dist/'));
+});
+
+gulp.task("nomnoml", function() {
+  var fs = require('fs');
+  var jison = require('jison');
+
+  var nomnomlParser = new jison.Parser(fs.readFileSync('nomnoml/nomnoml.jison', { encoding: 'utf8' }));
+  fs.writeFileSync('nomnoml/nomnoml.jison.js', nomnomlParser.generate({moduleName: 'nomnomlCoreParser'}));
+
+  var nomnomlFiles = [
+      'lib/dagre.min.js',
+      'nomnoml/nomnoml.jison.js',
+      'nomnoml/nomnoml.parser.js',
+      'nomnoml/nomnoml.layouter.js',
+      'nomnoml/nomnoml.renderer.js',
+      'nomnoml/nomnoml.js'
+  ];
+
+  function concat(files){
+      return files.map(function (filename){
+          return fs.readFileSync(filename, { encoding: 'utf8' })
+      }).join(';\n')
+  }
+
+  function replace(source, token, replacement){
+      return source.split(token).join(replacement)
+  }
+
+  var wrapper = fs.readFileSync('bundleWrapper.js', { encoding: 'utf8' })
+  var bundle = replace(wrapper, '/*{{body}}*/', concat(nomnomlFiles))
+
+  fs.writeFileSync('dist/nomnoml.js', bundle)
 });
 
