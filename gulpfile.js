7605826e96627065062e9e4fa940af6d5c3d24c5
Hakan Dilek <hakandilek@gmail.com>
2016-04-09 16:47:32 +0200

    install deps from gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,38 @@
-var gulp = require("gulp"),
-    browserSync = require('browser-sync').create(),
-    reload      = browserSync.reload;
+var gulp = require("gulp");
+gulp.task("install", function() {
+  var install = require("gulp-install");
+  return gulp.src(['./bower.json', './package.json'])
+    .pipe(install());
+});
 
-var webpack = require('webpack-stream');
+gulp.task('serve', ['webpack'], function() {
 
-gulp.task('serve', ['webpack'], function () {
+  var browserSync = require('browser-sync').create(),
+    reload = browserSync.reload;
 
-    // Serve files from the root of this project
-    browserSync.init({
-        server: {
-            baseDir: "./"
-        }
-    });
+  // Serve files from the root of this project
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
 
-    gulp.watch("index.html").on("change", reload);
-    gulp.watch("js/*.js").on("change", reload);
-    gulp.watch("codemirror/*").on("change", reload);
-    gulp.watch("nomnoml/*").on("change", reload);
-    gulp.watch("css/*.css").on("change", reload);
+  gulp.watch("index.html").on("change", reload);
+  gulp.watch("js/*.js").on("change", reload);
+  gulp.watch("codemirror/*").on("change", reload);
+  gulp.watch("nomnoml/*").on("change", reload);
+  gulp.watch("css/*.css").on("change", reload);
 });
 
 gulp.task('default', function() {
-    gulp.start('serve');
+  gulp.start('serve');
 });
 
 gulp.task("webpack", function() {
-return gulp.src('src/main.js')
-  .pipe(webpack( require('./webpack.config.js') ))
-  .pipe(gulp.dest('dist/'));
+  var webpack = require('webpack-stream');
+
+  return gulp.src('src/main.js')
+    .pipe(webpack(require('./webpack.config.js')))
+    .pipe(gulp.dest('dist/'));
 });
+
